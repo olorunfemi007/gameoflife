f4e9d75a1c338eda4495f7413b35b96638e3b7a8
Olorunfemi Kawonise <femi@MacBook-Pro.local>
2024-06-22 21:25:42 -0400

    stable version

--- ecr.tf
+++ ecr.tf
@@ -1,4 +1,4 @@
-resource "aws_ecr_repository" "java_app_repo" {
+resource "aws_ecr_repository" "calc_app_repo" {
   name = "calculator"
   image_scanning_configuration {
     scan_on_push = true
@@ -12,5 +12,22 @@ resource "aws_ecr_repository" "java_app_repo" {
 }
 
 output "repository_url" {
+  value = aws_ecr_repository.calc_app_repo.repository_url
+}
+
+resource "aws_ecr_repository" "java_app_repo" {
+  name = "java_app"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  image_tag_mutability = "MUTABLE"
+
+  tags = {
+    Name        = "ECRRepo"
+    Environment = "Dev"
+  }
+}
+
+output "repository_java_url" {
   value = aws_ecr_repository.java_app_repo.repository_url
 }
