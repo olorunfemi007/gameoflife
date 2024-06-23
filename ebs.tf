d542f63e0feecd2766ddc27370ffea63e09e30ed
Olorunfemi Kawonise <femi@MacBook-Pro.local>
2024-06-22 23:54:04 -0400

    finished

--- ebs.tf
+++
@@ -1,4 +0,0 @@
-# resource "aws_ebs_volume" "name" {
-#   availability_zone = var.region
-#   size = 10
-# }
--- ec2.tf
+++ ec2.tf
@@ -6,13 +6,6 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-# resource "aws_subnet" "private" {
-#   vpc_id = aws_vpc.main.id
-#   cidr_block = "10.0.0.0/24"
-#   map_public_ip_on_launch = true
-#   availability_zone = "us-east-1a"
-# }
-
 resource "aws_subnet" "public" {
   count = 2
   vpc_id = aws_vpc.main.id
@@ -41,45 +34,6 @@ resource "aws_route" "public_internet_access" {
   gateway_id = aws_internet_gateway.main.id
 }
 
-# resource "aws_eip" "nat_eip" {
-#   vpc = true
-# }
-
-# resource "aws_nat_gateway" "nat_gateway" {
-#   allocation_id = aws_eip.nat_eip.id
-#   subnet_id     = aws_subnet.public.id
-# }
-
-# # Create a route table for the private subnet
-# resource "aws_route_table" "private_route_table" {
-#   vpc_id = aws_vpc.main.id
-
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     nat_gateway_id = aws_nat_gateway.nat_gateway.id
-#   }
-# }
-
-# # Associate the private route table with the private subnet
-# resource "aws_route_table_association" "private_subnet_association" {
-#   count = 4
-#   subnet_id      = element(aws_subnet.private.*.id, count.index)
-#   route_table_id = aws_route_table.private_route_table.id
-# }
-
-
-# resource "aws_instance" "private_instance" {
-#   ami = var.ami
-#   key_name = var.key_pair
-#   instance_type = var.instance_type[0]
-#   subnet_id = aws_subnet.public.id
-#   user_data = file("${path.module}/jenkins.sh")
-#   vpc_security_group_ids  = [aws_security_group.public_sg.id]
-#   tags = {
-#     Name = "jumpserver"
-#   }
-# }
-
 resource "aws_instance" "public_instance" {
   ami = var.ami
   count = 2
@@ -93,16 +47,6 @@ resource "aws_instance" "public_instance" {
   }
 }
 
-# resource "aws_instance" "ec2" {
-#   ami           = var.ami
-#   instance_type = "t2.micro"
-#   key_name      = "vpc"
-#   user_data     = file("${path.module}/httpd.sh")
-#   tags = {
-#     Name = "terraform-ec2"
-#   }
-
-# }
 
 
 
